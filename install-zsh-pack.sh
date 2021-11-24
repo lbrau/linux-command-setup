69c8d374c388c2aea07bd394b09d3813afbef17f
Laurent BRAU <laurent.brau@gmail.com>
2021-11-24 12:54:14 +0100

    first commit for install

---
+++ install-zsh-pack.sh
@@ -0,0 +1,16 @@
+sudo apt update
+sudo apt upgrade
+# check version
+zsh --version
+sudo apt install zsh
+# check version
+zsh --version
+# set zsh to default shell
+chsh -s $(which zsh)
+sudo apt install git
+# get Oh my zsh
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+# set zsh to default shell
+chsh -s $(which zsh)
+
+
